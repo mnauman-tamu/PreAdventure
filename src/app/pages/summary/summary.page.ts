7fefdaa202d5b810a1038931ed42c42427815793
Brett Henry <wbh1942@tamu.edu>
2020-10-20 11:46:51 -0500

    listed weather

--- src/app/pages/summary/summary.page.ts
+++ src/app/pages/summary/summary.page.ts
@@ -3,31 +3,39 @@ import { DataService } from './../../shared/data.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'home-page',
+  selector: 'summary-page',
   templateUrl: './summary.page.html',
   styleUrls: ['./summary.page.scss']
 })
 export class SummaryPageComponent implements OnInit {
-  
   crimes: any[];
   POIs: any[];
+  forecast: any[];
 
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
       // Call APIs Here
-      this.dataService.crime().subscribe(
+      /*this.dataService.crime().subscribe(
         (data) => {
           console.log(data);
           this.crimes = data.results;
         }
-      )
+      );*/
 
-      this.dataService.tripAdvisorLocationSearch().subscribe(
+      /*this.dataService.tripAdvisorLocationSearch().subscribe(
         (data) => {
           console.log(data);
           this.POIs = data.data;
         }
-      )
+      );*/
+
+      this.dataService.fiveDayForecast().subscribe(
+        (data) => {
+          console.log(data);
+          this.forecast = data.list;
+        }
+      );
+
   }
 }
--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -3,7 +3,7 @@ import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 
-export interface search {
+export interface Search {
     from: string;
     to: string;
     start_date: string;
@@ -15,7 +15,7 @@ export interface search {
 })
 export class DataService {
 
-    search_input: search = {
+    search_input: Search = {
         from: '',
         to: '',
         start_date: '',
@@ -24,7 +24,7 @@ export class DataService {
 
     constructor(private http: HttpClient) {}
 
-    /* get request options 
+    /* get request options
     const options: {
         headers?: HttpHeaders | {[header: string]: string | string[]},
         observe?: 'body' | 'events' | 'response',
@@ -37,22 +37,8 @@ export class DataService {
 
     // https://angular.io/guide/http
 
-    spotifyByCountry(country: string): Observable<any> {
-        
-        let headers: HttpHeaders = new HttpHeaders();
-        headers = headers.append('Accept', 'application/json');
-        headers = headers.append(
-          'X-RapidAPI-Key',
-          '1108554cc1mshf11c17c4fea2b3dp179054jsn2446fb7a8965'
-        );
-        return this.http.get(
-          `https://restcountries-v1.p.rapidapi.com/capital/${this.search_input.from}` + name, 
-           {headers: headers}
-          )
-    }
-
     crime(): Observable<any> {
-        //let headers: HttpHeaders = new HttpHeaders();
+        // let headers: HttpHeaders = new HttpHeaders();
         /*headers = headers.append('Accept', 'application/json');
         headers = headers.append(
           'X-RapidAPI-Key',
@@ -60,25 +46,43 @@ export class DataService {
         );*/
         return this.http.get(
           `https://api.usa.gov/crime/fbi/sapi/api/summarized/state/TX/violent-crime/2000/2019?API_KEY=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv`
-          )
+          );
+    }
+
+    fiveDayForecast(): Observable<any> {
+      const options = {
+        headers: {
+          'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+          'x-rapidapi-key': 'a622ac738emsh154c26ff5de0944p1a36a0jsn6b447b8261a5'
+        },
+        params: {
+          q: `${this.search_input.to}, us`,
+          units: 'imperial'
+        }
+      };
+
+      return this.http.get(
+        `https://rapidapi.p.rapidapi.com/forecast`,
+        options
+      );
     }
 
     tripAdvisorLocationSearch(): Observable<any>
     {
-        let options = {
+        const options = {
             headers: {
                 'x-rapidapi-host': 'tripadvisor1.p.rapidapi.com',
                 'x-rapidapi-key': 'c240828760msh057482f498e41c4p172a21jsndb181050d689'
             },
             params: {
-                'query': `'${this.search_input.to}'`,
-                'location_id': '1',
-                'limit': '30',
-                'sort': 'relevance',
-                'offset': '0',
-                'lang': 'en_US',
-                'currency': 'USD',
-                'units': 'km'
+                query: `'${this.search_input.to}'`,
+                location_id: '1',
+                limit: '30',
+                sort: 'relevance',
+                offset: '0',
+                lang: 'en_US',
+                currency: 'USD',
+                units: 'km'
             }
         };
 
@@ -116,7 +120,7 @@ export class DataService {
 
 
 
-        
+
     }
 
     /*
