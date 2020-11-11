d0c68c1819f8963cda5520e30e02ae058a270b8e
Bryant Passage <bryant.passage@tamu.edu>
2020-11-11 10:14:38 -0600

    added test background for attractions page

--- src/app/pages/summary/summary.page.ts
+++ src/app/pages/summary/summary.page.ts
@@ -79,124 +79,124 @@ export class SummaryPageComponent implements OnInit {
 
     // trip advisor api servicing
     this.crimeDone = false;
-    if (this.dataStorage.needToRequest()) {
-      // this.dataService.tripAdvisorLocationSearch().subscribe(
-      //   (data) => {
-      //     // console.log(data);
-      //     for(var i=0; i<data.data.length; i++)
-      //     {
-      //       if(data.data[0].result_type == 'geos')
-      //       {
-      //         this.taLocationID = data.data[i].result_object.location_id;       //get location id to search other aspects
-      //         break;
-      //       }
-      //     }
-      //     console.log('Trip Advisor Location ID: ' + this.taLocationID);
-
-      //     //trip advisor attractions search
-      //     // const card = document.getElementById('AttractionsList')
-      //     this.dataService.tripAdvisorAttractionsSearch(this.taLocationID).subscribe(
-      //       (attractionsData) => {
-      //         console.log(attractionsData);
-      //         var j = 0;
-      //         for (var i = 0; i < attractionsData.data.length && i < 11; i++)
-      //         {
-      //           if(i == 6)
-      //             continue;
-      //           const attName = attractionsData.data[i].name;
-      //           const attDesc = attractionsData.data[i].description;
-      //           const attRate = attractionsData.data[i].rating;
-      //           const attAddy = attractionsData.data[i].address;
-      //           const attURL = attractionsData.data[i].website;
-      //           var attPhoto = null;
-      //           try
-      //           {
-      //             attPhoto = attractionsData.data[i].photo.images.large.url;
-      //           }
-      //           catch {}
-
-      //           let attObj = new DataClass.attObject(attName, attDesc, attRate, attAddy, attURL, attPhoto);
-      //           this.taAttractions[j] = attObj;
-
-      //           j++;
-      //         }
-      //         this.dataService.gettaAttractions(this.taAttractions);
-      //       }
-      //     );
-
-
-      //     //trip advisor hotels search api call
-      //     // const card2 = document.getElementById('HotelsList')
-      //     this.dataService.tripAdvisorHotelsSearch(this.taLocationID).subscribe(
-      //       (hotelsData) => {
-      //         console.log(hotelsData);
-      //         var j = 0;
-      //         for (var i = 0; i < hotelsData.data.length && i < 10; i++) {
-      //           const hotName = hotelsData.data[i].name;
-      //           const hotDesc = hotelsData.data[i].description;
-      //           const hotRate = hotelsData.data[i].rating;
-      //           const hotPrice = hotelsData.data[i].price;
-      //           const hotAddy = hotelsData.data[i].address;
-      //           const hotURL = hotelsData.data[i].web_url;
-      //           var hotWifi = false;
-      //           var hotBreakfast = false;
-      //           var hotPhoto = null;
-      //           try {
-      //             hotPhoto = hotelsData.data[i].photo.images.large.url;
-      //           }
-      //           catch { }
-
-      //           //search through amenities
-      //           for(let dict of hotelsData.data[i].amenities)
-      //           {
-      //             if(dict.name.search(/wifi/i) > -1)
-      //             {
-      //               hotWifi = true;
-      //             }
-      //             if(dict.name.search(/breakfast/i) > -1)
-      //             {
-      //               hotBreakfast = true;
-      //             }
-      //           }
-
-      //           let hotObj = new DataClass.hotObject(hotName, hotDesc, hotRate, hotPrice, hotAddy, hotURL, hotPhoto, hotWifi, hotBreakfast);
-      //           this.taHotels[i] = hotObj;
-      //         }
-      //         this.dataService.gettaHotels(this.taHotels);
-      //       }
-      //     );
-
-      //     //trip advisor restaurants search api call
-      //     this.dataService.tripAdvisorRestaurantSearch(this.taLocationID).subscribe(
-      //       (restData) => {
-      //         console.log(restData);
-      //         var j = 0;
-      //         for (var i = 0; i < restData.data.length && i < 11; i++) {
-      //           if(i == 4)
-      //             continue;
-      //           const restName = restData.data[i].name;
-      //           const restDesc = restData.data[i].description;
-      //           const restRating = restData.data[i].rating;
-      //           const restPrice = restData.data[i].price_level;
-      //           const restAddy = restData.data[i].address;
-      //           const restURL = restData.data[i].website;
-      //           var restPhoto = null;
-      //           try {
-      //             restPhoto = restData.data[i].photo.images.large.url;
-      //           }
-      //           catch { }
-
-      //           let restObj = new DataClass.restObject(restName, restDesc, restRating, restPrice, restAddy, restURL, restPhoto);
-      //           this.taRestaurants[j] = restObj;
-
-      //           j++;
-      //         }
-      //         this.dataService.gettaRestaurants(this.taRestaurants);
-      //         this.dataStorage.summaryPageAPIs(this);
-      //       }
-      //     );
-      //   }
-      // );
+    if(this.dataStorage.needToRequest()) {
+      this.dataService.tripAdvisorLocationSearch().subscribe(
+        (data) => {
+          // console.log(data);
+          for(var i=0; i<data.data.length; i++)
+          {
+            if(data.data[0].result_type == 'geos')
+            {
+              this.taLocationID = data.data[i].result_object.location_id;       //get location id to search other aspects
+              break;
+            }
+          }
+          console.log('Trip Advisor Location ID: ' + this.taLocationID);
+      
+          //trip advisor attractions search
+          // const card = document.getElementById('AttractionsList')
+          this.dataService.tripAdvisorAttractionsSearch(this.taLocationID).subscribe(
+            (attractionsData) => {
+              console.log(attractionsData);
+              var j = 0;
+              for (var i = 0; i < attractionsData.data.length && i < 11; i++)
+              {
+                if(i == 6)
+                  continue;
+                const attName = attractionsData.data[i].name;
+                const attDesc = attractionsData.data[i].description;
+                const attRate = attractionsData.data[i].rating;
+                const attAddy = attractionsData.data[i].address;
+                const attURL = attractionsData.data[i].website;
+                var attPhoto = null;
+                try
+                {
+                  attPhoto = attractionsData.data[i].photo.images.large.url;
+                }
+                catch {}
+      
+                let attObj = new DataClass.attObject(attName, attDesc, attRate, attAddy, attURL, attPhoto);
+                this.taAttractions[j] = attObj;
+      
+                j++;
+              }
+              this.dataService.gettaAttractions(this.taAttractions);
+            }
+          );
+      
+      
+          //trip advisor hotels search api call
+          // const card2 = document.getElementById('HotelsList')
+          this.dataService.tripAdvisorHotelsSearch(this.taLocationID).subscribe(
+            (hotelsData) => {
+              console.log(hotelsData);
+              var j = 0;
+              for (var i = 0; i < hotelsData.data.length && i < 10; i++) {
+                const hotName = hotelsData.data[i].name;
+                const hotDesc = hotelsData.data[i].description;
+                const hotRate = hotelsData.data[i].rating;
+                const hotPrice = hotelsData.data[i].price;
+                const hotAddy = hotelsData.data[i].address;
+                const hotURL = hotelsData.data[i].web_url;
+                var hotWifi = false;
+                var hotBreakfast = false;
+                var hotPhoto = null;
+                try {
+                  hotPhoto = hotelsData.data[i].photo.images.large.url;
+                }
+                catch { }
+      
+                //search through amenities
+                for(let dict of hotelsData.data[i].amenities)
+                {
+                  if(dict.name.search(/wifi/i) > -1)
+                  {
+                    hotWifi = true;
+                  }
+                  if(dict.name.search(/breakfast/i) > -1)
+                  {
+                    hotBreakfast = true;
+                  }
+                }
+      
+                let hotObj = new DataClass.hotObject(hotName, hotDesc, hotRate, hotPrice, hotAddy, hotURL, hotPhoto, hotWifi, hotBreakfast);
+                this.taHotels[i] = hotObj;
+              }
+              this.dataService.gettaHotels(this.taHotels);
+            }
+          );
+      
+          //trip advisor restaurants search api call
+          this.dataService.tripAdvisorRestaurantSearch(this.taLocationID).subscribe(
+            (restData) => {
+              console.log(restData);
+              var j = 0;
+              for (var i = 0; i < restData.data.length && i < 11; i++) {
+                if(i == 4)
+                  continue;
+                const restName = restData.data[i].name;
+                const restDesc = restData.data[i].description;
+                const restRating = restData.data[i].rating;
+                const restPrice = restData.data[i].price_level;
+                const restAddy = restData.data[i].address;
+                const restURL = restData.data[i].website;
+                var restPhoto = null;
+                try {
+                  restPhoto = restData.data[i].photo.images.large.url;
+                }
+                catch { }
+      
+                let restObj = new DataClass.restObject(restName, restDesc, restRating, restPrice, restAddy, restURL, restPhoto);
+                this.taRestaurants[j] = restObj;
+      
+                j++;
+              }
+              this.dataService.gettaRestaurants(this.taRestaurants);
+              this.dataStorage.summaryPageAPIs(this);
+            }
+          );
+        }
+      );
 
         // mapquest api servicing
 
